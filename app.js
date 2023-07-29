a10517cae379c2ad00a9d8ea267507b404647e57
grezisek <38856057+grezisek@users.noreply.github.com>
2023-07-29 19:43:54 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,211 +1,279 @@
-const apiKey = "ddafNA5xyFLZKvHdMKR1mI6S2e91iX3rhJhTr6b8XgdH4fT4OhtPUMVOVK0mqjcW";
-const pi2 = Math.PI * 2;
-const radius = 0.05;
-
-const state = {
-    imageCtx: wajbuj_panel_canvas.getContext("2d"),
-    audioCtx: null,
-    audioSources: [],
-    masterGainNode: null,
-    gainNode: null,
-    
-    user: null,
-    position: 0,
-    wave: Math.round((wave.valueAsNumber ?? 0) * 10),
-    
-    loopFrame: null,
-    data: [],
-    abortController: null,
-
-    timeRemaining: 1000,
-    lastTimestamp: 0,
-
-    request: null,
+const Accordion = () => {
+    document.querySelectorAll(".accordion__summary").forEach(node => node.classList.add("has-mouse-effects"));
 };
 
-const UpdatePosition = (changeEvent) => {
-    state.position = changeEvent.target.valueAsNumber ?? 0;
-    wajbuj_panel_position.value = state.position;
-    wajbuj_panel_position_slider.value = state.position;
+const ScrollbarWidthVariableInCSS = () => {
+    const probe = document.createElement("div");
+    probe.style.setProperty("overflow", "scroll");
+    probe.style.setProperty("position", "absolute");
+    probe.style.setProperty("left", "-300vw");
+    document.body.appendChild(probe);
+    document.documentElement.style.setProperty("--scrollbar_width", probe.offsetWidth.toString());
+    document.body.removeChild(probe);
 };
-const UpdateWave = (changeEvent) => {
-    state.wave = Math.round((changeEvent.target.valueAsNumber ?? 0) * 10);
-    wajbuj_panel_wave.value = changeEvent.target.valueAsNumber;
-    wajbuj_panel_wave_slider.value = changeEvent.target.valueAsNumber;
+const ResizeRWDVariableInCSS = () => {
+    const computedStyle = window.getComputedStyle(document.documentElement);
+    const min = parseFloat(computedStyle.getPropertyValue("--screen_scaling_minimum"));
+    const max = parseFloat(computedStyle.getPropertyValue("--screen_scaling_maximum"));
+    const current = parseFloat(window.getComputedStyle(document.documentElement, "::before").getPropertyValue("width"));
+    const value = Math.min(1, Math.max(0, current - min) / (max - min));
+    document.documentElement.style.setProperty("--rwd_state", value.toString());
 };
-const UpdateVolume = (changeEvent) => state.masterGainNode.gain.value = changeEvent.target.valueAsNumber / 100;
-const ResizeCanvas = () => {
-    const canvasRect = wajbuj_panel_canvas.getBoundingClientRect();
-    wajbuj_panel_canvas.width = Math.floor(canvasRect.width * devicePixelRatio);
-    wajbuj_panel_canvas.height = Math.floor(canvasRect.height * devicePixelRatio);
+const UpdateBodyHasScrollbar = () => {
+    document.body.classList.toggle("is-scrollbar-visible", window.innerWidth > document.documentElement.clientWidth);
+};
+const CSSVars = () => {
+    ScrollbarWidthVariableInCSS();
+    ResizeRWDVariableInCSS();
+    UpdateBodyHasScrollbar();
+    window.addEventListener("resize", ScrollbarWidthVariableInCSS, { passive: true });
+    window.addEventListener("resize", ResizeRWDVariableInCSS, { passive: true });
+    window.addEventListener("resize", UpdateBodyHasScrollbar, { passive: true });
 };
 
-const Start = async () => {
-    if (!wave.validity.valid || !state.wave) {
-        alert("Wybierz falę");
+const ContentEntry = () => {
+    document.querySelectorAll(".content-entry").forEach(node => node.classList.add("has-mouse-effects"));
+};
+
+const mouseEffectsNodes = [];
+const UpdateMouseEffectsSubscriber = (subscriber, mouseMoveEvent) => {
+    const rect = subscriber.getBoundingClientRect();
+    const top = mouseMoveEvent.clientY - rect.top;
+    const left = mouseMoveEvent.clientX - rect.left;
+    subscriber.style.setProperty('--cursor-top--px', `${top}px`);
+    subscriber.style.setProperty('--cursor-left--px', `${left}px`);
+    subscriber.style.setProperty('--cursor-top--percent', `${top / (subscriber.offsetHeight ?? subscriber.clientHeight) * 100}%`);
+    subscriber.style.setProperty('--cursor-left--percent', `${left / (subscriber.offsetWidth ?? subscriber.clientWidth) * 100}%`);
+    subscriber.classList.add("mouse-effects-set");
+};
+const UpdateMouseEffects = (mouseMoveEvent) => {
+    for (const node of mouseEffectsNodes) {
+        UpdateMouseEffectsSubscriber(node, mouseMoveEvent);
+    }
+};
+const MouseEffects = () => {
+    if ("ontouchstart" in document.documentElement) {
         return;
     }
-
-    state.user = fetch("https://eu-central-1.aws.data.mongodb-api.com/app/wajbuj-cjyqh/endpoint/wajbuj", {
-        method: "POST",
-        cache: "no-cache",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ "api-key" : apiKey, w: state.wave }),
+    document.querySelectorAll(".has-mouse-effects, button").forEach((subscriber) => {
+        if (mouseEffectsNodes.includes(subscriber)) {
+            return;
+        }
+        mouseEffectsNodes.push(subscriber);
     });
-
-    wajbuj_panel_wave.value = state.wave / 10;
-    wajbuj_panel_wave_slider.value = state.wave / 10;
-
-    wajbuj_panel_position.value = state.position;
-    wajbuj_panel_position_slider.value = state.position;
-
-    wajbuj_panel.showModal();
-    document.body.classList.add("wajbuj-open");
-
-    state.timeRemaining = 1000;
-    state.lastTimestamp = performance.now();
-
-    state.user = await state.user;
-
-    if (!state.user.ok) {
-        alert("Błąd połączenia");
-        console.debug(state.user.text());
-        return;
-    }
-
-    if (state.audioCtx === null) {
-        state.audioCtx = new AudioContext();
-        state.masterGainNode = state.audioCtx.createGain();
-        state.masterGainNode.connect(state.audioCtx.destination);
-        state.gainNode = state.audioCtx.createGain();
-        state.gainNode.connect(state.masterGainNode);
-    }
-
-    state.user = await state.user.json();
-    state.loopFrame = requestAnimationFrame(Loop);
-
-    state.masterGainNode.gain.value = wajbuj_panel_volume.valueAsNumber / 100;
-};
-const Stop = () => {
-    cancelAnimationFrame(state.loopFrame);
-    state.loopFrame = null;
-
-    for (let i = state.audioSources.length - 1; i >= 0; i--) {
-        state.audioSources[i][0].stop();
-        state.audioSources[i][0].disconnect();
-        state.audioSources[i][1].disconnect();
-        state.audioSources.pop();
-    }
-
-    if (state.abortController !== null) {
-        state.abortController.abort();
-        state.abortController = null;
-    }
-};
-const Loop = () => {
-    state.timeRemaining -= performance.now() - state.lastTimestamp;
-
-    if (state.timeRemaining <= 0) {
-        TimePassed();
-        state.timeRemaining = 1000 - state.timeRemaining;
-    }
-
-    state.lastTimestamp = performance.now();
-    state.loopFrame = requestAnimationFrame(Loop);
+    addEventListener("mousemove", UpdateMouseEffects, { passive: true });
 };
 
-const TimePassed = async () => {
-    state.data = await Wajbuj();
-
-    for (let i = state.audioSources.length; i < state.data.length; i++) {
-        state.audioSources[i] = [
-            state.audioCtx.createOscillator(),
-            state.audioCtx.createStereoPanner(),
-        ];
-        state.audioSources[i][0].type = "sine";
-        state.audioSources[i][0].connect(state.audioSources[i][1]);
-        state.audioSources[i][1].connect(state.gainNode);
-        state.audioSources[i][0].start();
-    }
-
-    for (let i = state.audioSources.length - 1; i > state.data.length - 1; i--) {
-        state.audioSources[i][0].stop();
-        state.audioSources[i][0].disconnect();
-        state.audioSources[i][1].disconnect();
-        state.audioSources.pop();
-    }
-
-    let x, y;
-    let r = Math.min(state.imageCtx.canvas.width, state.imageCtx.canvas.height) * radius / 2;
-    
-    state.imageCtx.clearRect(0, 0, state.imageCtx.canvas.width, state.imageCtx.canvas.height);
-    state.imageCtx.fillStyle = window.getComputedStyle(document.body).getPropertyValue("--accent");
-    state.imageCtx.beginPath();
-    for (let i = 0; i < state.data.length; i++) {
-        state.audioSources[i][0].frequency.setValueAtTime(state.data[i][0], state.audioCtx.currentTime);
-        state.audioSources[i][1].pan.setValueAtTime(-state.data[i][1], state.audioCtx.currentTime);
-        
-        x = (1 - (state.data[i][1] + 1) / 2) * state.imageCtx.canvas.width;
-        y = (1 - state.data[i][0] / 20000) * state.imageCtx.canvas.height;
-        
-        state.imageCtx.moveTo(x + r, y);
-        state.imageCtx.arc(x, y, r, 0, pi2);
-    }
-    state.imageCtx.fill();
-
-    state.gainNode.gain.value = state.audioSources.length ? 1 / state.audioSources.length : 1;
-
-    wajbuj_panel_count.value = state.data.length;
+const settingsData = {
+    border_width: "",
+    theme: "",
+    contrast: "",
+    data: "",
 };
-
-const Wajbuj = async () => {
-    state.abortController = new AbortController();
-
-    let data = await fetch("https://eu-central-1.aws.data.mongodb-api.com/app/wajbuj-cjyqh/endpoint/w", {
-        method: "POST",
-        cache: "no-cache",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ "api-key" : apiKey, d: `${state.user}${Math.floor((state.position + 1) / 2 * 99).toString().padStart(2, "0")}${state.wave}` }),
-        signal: state.abortController.signal
+const SettingsUpdateForm = () => {
+    for (const settingKey of Object.keys(settingsData)) {
+        if (window["settings_form"].elements[settingKey][0]) {
+            for (let nodeIndex = 0; nodeIndex < window["settings_form"].elements[settingKey].length; nodeIndex++) {
+                window["settings_form"].elements[settingKey][nodeIndex].checked =
+                    window["settings_form"].elements[settingKey][nodeIndex].value == settingsData[settingKey];
+            }
+        }
+        else {
+            window["settings_form"].elements[settingKey].value = settingsData[settingKey];
+        }
+    }
+};
+const Settings = () => {
+    SettingsModal();
+    SettingsUpdateForm();
+    // SettingsApply();
+    window["settings_output"].textContent = sessionStorage.getItem("settings") ?? localStorage.getItem("settings") ?? "";
+    SettingsListeners();
+    window["settings_delete"].addEventListener("click", (clickEvent) => {
+        localStorage.removeItem("settings");
+        sessionStorage.removeItem("settings");
+        for (const settingKey of Object.keys(settingsData)) {
+            settingsData[settingKey] = "";
+        }
+        window["settings_output"].textContent = "";
+        SettingsUpdateForm();
     });
-
-    if (!data.ok) {
-        console.debug(request);
+    window["settings_form"].addEventListener("submit", submitEvent => submitEvent.preventDefault());
+};
+const SettingsModal = () => {
+    window["settings_open"].addEventListener("click", clickEvent => {
+        window["settings_panel"].showModal();
+        document.body.classList.add("is-settings-open");
+        window["settings_panel_content"].scrollTo(0, 0);
+    });
+    window["settings_close"].addEventListener("click", clickEvent => {
+        window["settings_panel"].close();
+        document.body.classList.remove("is-settings-open");
+    });
+};
+const SettingsLoad = () => {
+    const loadedSettingsData = JSON.parse(sessionStorage.getItem("settings") ?? localStorage.getItem("settings") ?? "{}");
+    for (const settingKey of Object.keys(settingsData)) {
+        settingsData[settingKey] = loadedSettingsData[settingKey] ?? "";
+    }
+};
+const SettingsSave = () => {
+    const saveData = {};
+    for (const [settingKey, settingValue] of Object.entries(settingsData)) {
+        if (!settingValue) {
+            continue;
+        }
+        saveData[settingKey] = settingValue;
+    }
+    if (!Object.keys(saveData).length) {
         return;
     }
-
-    data = await data.json();
-
-    state.abortController = null;
-
-    if (data == "") {
-        wajbuj_panel.close();
-        return [];
+    if (settingsData.data == "local_storage") {
+        localStorage.setItem("settings", JSON.stringify(saveData));
     }
-
-    data = data.split(",");
-
-    for (const dataIndex in data) {
-        data[dataIndex] = [parseInt(data[dataIndex].slice(2)) / 10, parseInt(data[dataIndex].slice(0, 2)) * 2 / -99 + 1];
+    else {
+        sessionStorage.setItem("settings", JSON.stringify(saveData));
     }
-
-    return data;
+};
+const SettingsApply = () => {
+    document.documentElement.classList.toggle("colorscheme", !!settingsData.theme);
+    document.documentElement.classList.toggle("colorscheme--dark", settingsData.theme == "dark");
+    document.documentElement.classList.toggle("colorscheme--light", settingsData.theme == "light");
+    document.documentElement.classList.toggle("colorscheme--contrast-high", settingsData.contrast == "more");
+    document.documentElement.classList.toggle("colorscheme--contrast-low", settingsData.contrast == "less");
+    if (settingsData.border_width) {
+        document.body.style.setProperty("--border_width", `var(--border_width_${settingsData.border_width})`);
+    }
+    else {
+        document.body.style.removeProperty("--border_width");
+    }
+};
+const OnSettingsChange = (changeEvent) => {
+    localStorage.removeItem("settings");
+    sessionStorage.removeItem("settings");
+    settingsData[changeEvent.target.name] = changeEvent.target.value ?? "";
+    if (settingsData.data) {
+        SettingsSave();
+    }
+    SettingsApply();
+    window["settings_output"].textContent = sessionStorage.getItem("settings") ?? localStorage.getItem("settings") ?? "";
+};
+const SettingsListeners = () => {
+    for (const settingKey of Object.keys(settingsData)) {
+        if (window["settings_form"].elements[settingKey][0]) {
+            for (let nodeIndex = 0; nodeIndex < window["settings_form"].elements[settingKey].length; nodeIndex++) {
+                window["settings_form"].elements[settingKey][nodeIndex].addEventListener("change", OnSettingsChange);
+            }
+        }
+        else {
+            window["settings_form"].elements[settingKey].addEventListener("change", OnSettingsChange);
+        }
+    }
 };
 
-wave.addEventListener("change", UpdateWave);
-wajbuj_panel_volume.addEventListener("change", UpdateVolume);
+const SyntaxBackground = () => {
+    document.querySelectorAll("pre code").forEach(node => node.classList.add("has-mouse-effects"));
+};
 
-wajbuj_panel_wave.addEventListener("change", UpdateWave);
-wajbuj_panel_wave_slider.addEventListener("change", UpdateWave);
-wajbuj_panel_position.addEventListener("change", UpdatePosition);
-wajbuj_panel_position_slider.addEventListener("change", UpdatePosition);
+const ViewTransitions = () => {
+    if (!("startViewTransition" in document)) {
+        return;
+    }
+    let hashIndex = location.href.lastIndexOf("#");
+    let lastLink = hashIndex == -1 ? location.href : location.href.slice(0, hashIndex);
+    const linksQuery = "a:not(.skip-link):not([href^='mailto']):not(.no-view-transition)";
+    let tempHrefPrevious = null;
+    let tempHref = null;
+    let tempAbortController = null;
+    const UpdateView = async (href, pushState = false) => {
+        document.body.classList.add("is-loading");
+        try {
+            const viewTransition = document.startViewTransition(async () => {
+                tempHref = href;
+                tempAbortController = new AbortController();
+                let request = fetch(tempHref, { signal: tempAbortController.signal });
+                if (pushState) {
+                    history.pushState(null, "", href);
+                }
+                const header = document.querySelector(".site__header");
+                const content = document.querySelector(".site__content");
+                const tempNode = document.createElement("div");
+                request = await request;
+                if (!request.ok) {
+                    document.title = "404";
+                    content.innerHTML = "<h1>404</h1>";
+                    throw new Error("404");
+                }
+                tempNode.innerHTML = await request.text();
+                tempHref = null;
+                tempAbortController = null;
+                header.replaceWith(tempNode.querySelector(".site__header"));
+                content.replaceWith(tempNode.querySelector(".site__content"));
+                document.title = tempNode.querySelector("title").textContent;
+                UpdateBodyHasScrollbar();
+            });
+            await viewTransition.updateCallbackDone;
+            window.scrollTo(0, 0);
+            Accordion();
+            ContentEntry();
+            SyntaxBackground();
+            MouseEffects();
+            window["hljs"]?.highlightAll();
+            document.querySelectorAll(linksQuery)
+                .forEach(link => link.addEventListener("click", HandleNavigationLinkClicked));
+            tempHrefPrevious = location.href;
+            document.documentElement.dispatchEvent(new CustomEvent("viewchange"));
+        }
+        catch (error) {
+            console.debug(error);
+        }
+        document.body.classList.remove("is-loading");
+    };
+    const HandleNavigationLinkClicked = (clickEvent) => {
+        if ((clickEvent.target.host ?? location.host) != location.host) {
+            return;
+        }
+        const hashIndex = location.href.lastIndexOf("#");
+        const newLink = hashIndex == -1 ? clickEvent.target.href : clickEvent.target.href.slice(0, hashIndex);
+        if (lastLink == newLink) {
+            return;
+        }
+        lastLink = newLink;
+        clickEvent.preventDefault();
+        tempHrefPrevious = location.href;
+        UpdateView(clickEvent.target.href, true);
+    };
+    const HandleNavigationPopstate = (popstateEvent) => {
+        const hashIndex = location.href.lastIndexOf("#");
+        const newLink = hashIndex == -1 ? location.href : location.href.slice(0, hashIndex);
+        if (lastLink == newLink) {
+            return;
+        }
+        lastLink = newLink;
+        popstateEvent.preventDefault();
+        if (tempHref !== null) {
+            tempAbortController.abort();
+            tempHref = null;
+            if (tempHrefPrevious == location.href) {
+                return;
+            }
+        }
+        UpdateView(location.href);
+    };
+    document.querySelectorAll(linksQuery)
+        .forEach(link => link.addEventListener("click", HandleNavigationLinkClicked));
+    window.addEventListener("popstate", HandleNavigationPopstate);
+};
 
-wajbuj_panel.addEventListener("close", () => document.body.classList.remove("wajbuj-open"));
-wajbuj_panel.addEventListener("close", Stop);
-wajbuj_panel_close.addEventListener("click", () => wajbuj_panel.close());
-wajbuj.addEventListener("click", Start);
-window.addEventListener("resize", ResizeCanvas, { passive: true });
-setTimeout(ResizeCanvas, 50);
-
-let hue = Math.random() * 240 + 60;
-setInterval(() => {document.documentElement.style.setProperty("--accent-hue", hue = (hue+0.05) % 360)}, 100);
+document.addEventListener("readystatechange", _ => {
+    if (document.readyState != "complete") {
+        return;
+    }
+    Accordion();
+    ContentEntry();
+    SyntaxBackground();
+    MouseEffects();
+    ViewTransitions();
+    Settings();
+});
+SettingsLoad();
